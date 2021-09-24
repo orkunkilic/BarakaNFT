84a1b86702cf1d9b3a2bbbd52837638f330c7c67
Orkun M. KILIÇ <orkun.kilic@boun.edu.tr>
2021-09-24 15:20:16 +0300

    delete unnecesary funcs

--- src/contracts/NFTApp.sol
+++ src/contracts/NFTApp.sol
@@ -56,49 +56,4 @@ contract NFTApp {
 
   }
 
-
-  /* function withdraw() public {
-    require(isDeposited[msg.sender] == true, "Error, no previous deposit");
-    
-    uint userBalance = etherBalanceOf[msg.sender];
-    uint depositTime = block.timestamp - depositStart[msg.sender];
-
-    uint interestPerSecond = 31668017 * (userBalance / 1e16);
-    uint interest = interestPerSecond * depositTime;
-
-    msg.sender.transfer(userBalance);
-    token.mint(msg.sender, interest);
-
-    etherBalanceOf[msg.sender] = 0;
-    depositStart[msg.sender] = 0;
-    isDeposited[msg.sender] = false;
-
-    emit Withdraw(msg.sender, userBalance, depositTime, interest);
-  }
-
-  function borrow() payable public {
-    require(msg.value >= 1e16, "Error, deposit must be >= 0.1 ETH");
-    require(isBorrowed[msg.sender] == false, "Error, user has an active loan!");
-
-    etherCollateral[msg.sender] = etherCollateral[msg.sender] + msg.value;
-    uint tokenAmountToMint = msg.value / 2;
-    token.mint(msg.sender, tokenAmountToMint);
-
-    isBorrowed[msg.sender] = true;
-
-    emit Borrow(msg.sender, etherCollateral[msg.sender], tokenAmountToMint);
-  }
-
-  function payOff() public {
-    require(isBorrowed[msg.sender] == true, "Error, user does not have an active loan!");
-    require(token.transferFrom(msg.sender, address(this), etherCollateral[msg.sender] / 2), "Error, can't receive tokens.");
-
-    uint fee = etherCollateral[msg.sender] / 10;
-    msg.sender.transfer(etherCollateral[msg.sender] - fee);
-
-    etherCollateral[msg.sender] = 0;
-    isBorrowed[msg.sender] = false;
-
-    emit PayOff(msg.sender, fee);
-  } */
 }
